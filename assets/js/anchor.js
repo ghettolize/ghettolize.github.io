14a644be4cbb961f41948f30304aee7a8ae81da9
ghettolize <ghettolize@gmail.com>
2017-08-19 00:42:29 +0700

    Update anchor.js

--- assets/js/anchor.js
+++ assets/js/anchor.js
@@ -2,7 +2,7 @@ $(document).ready(function() {
 $('a[href^="#"]').click(function(){
 var el = $(this).attr('href');
 $('body').animate({
-scrollTop: $(el).offset().top}, 2000);
+scrollTop: $(el).offset().top}, 1000);
 return false;
 });
 });
