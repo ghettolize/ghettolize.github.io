628e447c60d45d3c32da0b473b88c4b7e6844bb1
ghettolize <ghettolize@gmail.com>
2017-08-21 17:01:34 +0700

    Update anchor.js

--- assets/js/anchor.js
+++ assets/js/anchor.js
@@ -1,15 +1,3 @@
-/*
-$(document).ready(function() {
-$('a[href^="#"]').click(function(){
-var el = $(this).attr('href');
-$('body').animate({
-scrollTop: $(el).offset().top}, 700);
-return false;
-});
-});
-
-*/
-
 $(document).ready(function(){
 $('a[href*=#]').bind("click", function(e){
 var anchor = $(this);
