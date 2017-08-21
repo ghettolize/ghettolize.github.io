1c67f28c3d1306e7c8c27186e08e89626ed9d4dd
ghettolize <ghettolize@gmail.com>
2017-08-21 17:00:24 +0700

    Update anchor.js

--- assets/js/anchor.js
+++ assets/js/anchor.js
@@ -10,13 +10,13 @@ return false;
 
 */
 
-$(document).ready(function() {
-  $("a.scrollto").click(function() {
-    var elementClick = $(this).attr("href")
-    var destination = $(elementClick).offset().top;
-    jQuery("html:not(:animated),body:not(:animated)").animate({
-      scrollTop: destination
-    }, 800);
-    return false;
-  });
+$(document).ready(function(){
+$('a[href*=#]').bind("click", function(e){
+var anchor = $(this);
+$('html, body').stop().animate({
+scrollTop: $(anchor.attr('href')).offset().top
+}, 1000);
+e.preventDefault();
+});
+return false;
 });
